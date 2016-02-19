72df88152c4be6cfa822a6fa2df3e4bfcdb4808e
Keisuke Ota <arvelt.s@gmail.com>
2016-02-20 05:19:38 +0900

    CLI風にした

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import click
 from states_store import StateStore
 from weather_store import WeatherStore
 
@@ -8,9 +8,20 @@ states = StateStore()
 #print states.get_city_id_by_name(u'銚子')
 
 city = states.get_city_id_by_name(u'東京')
-weather = WeatherStore(city=city)
-weather.get_weather_description()
+weatherStore = WeatherStore(city=city)
 
-print weather.get_weather_today()
-print weather.get_weather_tomorrow()
-print weather.get_weather_day_after_tomorrow()
+@click.command()
+@click.option('--today', '-t', 'scope', flag_value='today', help='Show the today weather')
+@click.option('--tomorrow', '-tt', 'scope', flag_value='tomorrow', help='Show the tomorrow weather')
+@click.option('--aftertomorrow', '-ttt', 'scope', flag_value='aftertomorrow', help='Show the day after tomorrow weather')
+def weather(scope):
+    empty_messagy = 'Nothing forecasts'
+    if scope == 'tomorrow':
+        print weatherStore.get_weather_tomorrow() or empty_messagy
+    elif scope == 'aftertomorrow':
+        print weatherStore.get_weather_day_after_tomorrow() or empty_messagy
+    else:
+        print weatherStore.get_weather_today() or empty_messagy
+
+if __name__ == '__main__':
+    weather()
